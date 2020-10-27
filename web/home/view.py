e16382f0a542631919f40c00b7a938064120cb2c
Jason Kuan (Astra) <jason9075@gmail.com>
2020-10-27 15:27:14 +0800

    feat add server status
     add server status

--- web/home/view.py
+++ web/home/view.py
@@ -1,9 +1,22 @@
+import os
+from distutils.util import strtobool
+
 from flask import Blueprint, render_template
+from mcstatus import MinecraftServer
 
 home = Blueprint('home', __name__, template_folder='pages', static_folder='static')
 
 
 @home.route('/')
-@home.route('/home')
 def index():
-    return render_template('main.html')
+    server = MinecraftServer.lookup(os.getenv("MC_SERVER_HOST"))
+    status = server.status()
+    players = ""
+    if strtobool(os.getenv("ENABLE_QUERY")):
+        query = server.query()
+        players = "\n".join(query.players.names)
+    return render_template('main.html',
+                           version=status.version.name,
+                           max_number=status.players.max,
+                           current=status.players.online,
+                           players=players)
